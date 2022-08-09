d0c63a4d9d684d0469f7d754c4e2710dd76cc0c8
nabilghi55 <89720241+nabilghi55@users.noreply.github.com>
2022-08-09 11:00:16 +0700

    Add files via upload

---
+++ kb2/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage : (theme) =>({
+        'bgbawah-img': "url('assets/bgbawah.png)",
+      }),
+    },
+  },
+  plugins: [],
+}
